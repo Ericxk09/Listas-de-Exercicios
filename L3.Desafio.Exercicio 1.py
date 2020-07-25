e775a53f76acaea0e6a22e0b01d20701874a17b9
Eric <68623578+Ericxk09@users.noreply.github.com>
2020-07-24 23:16:43 -0300

    L3.Desafio.Exercicio 1
    Dizemos que um número natural é triangular se ele é produto de três números naturais
    consecutivos. Exemplo: 120 é triangular, pois 4.5.6 = 120. Dado um inteiro não-negativo n,
    verificar se n é triangular.

---
+++ L3.Desafio.Exercicio 1.py
@@ -0,0 +1,32 @@
+n = 1
+print("Digite zero ou qualquer valor para parar o programa")
+
+while n >= 1:
+    
+    n = int(input("Digite um numero: "))
+    a = 1
+    b = a + 1
+    c = b + 1
+    x = a * b * c
+
+    while x != n or x == n:
+        if x > n and n >= 1:
+            print("Numero não é triangular")
+            print("Digite zero ou valor para parar o programa")
+            print()
+            break
+        elif x == n:
+            print('A: ',a)
+            print("B: ",b)
+            print("C: ",c)
+            print(f"A({a}) * B({b})* C({c}) = ", x)
+            print("Numero Triangular")
+            break
+        elif n <= 0:
+            print("Loop encerrado!")
+            break
+        a = a + 1
+        b = a + 1
+        c = b + 1
+        x = a * b * c
+            
